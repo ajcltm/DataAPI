8eb07965f6291f03cb521da94346528340ca910b
ajcltm <ajcltm@gmail.com>
2021-12-19 23:32:12 +0900

    2021.12.19. 23:32

--- report.py
+++ report.py
@@ -85,10 +85,10 @@ class Handler:
         r = requests.get(url, params=detailReportParameter[0])
         html = r.text
         soup = BeautifulSoup(html, 'html.parser')
-        table = soup.select('table')
+        table = soup.find_all('table')
         if table :
             print('got table')
-            return pd.read_html(html)
+            return soup
         elif self.successor is not None:
             print('successor')
             return self.successor.handle_request()
@@ -110,7 +110,7 @@ class Handler:
             print('None')
             return None
 
-class Report:
+class HtmlReport:
 
     def get_report(self, rcept_no):
         url = f'http://dart.fss.or.kr/dsaf001/main.do?rcpNo={rcept_no}'
@@ -118,14 +118,62 @@ class Report:
         reportHtml = r.text
         parser1 = Parser(nord = Nord2Parser(reportHtml), parser_format = r'.*연결재무제표$')
         parser2 = Parser(nord = Nord1Parser(reportHtml), parser_format = r'.*재무제표 등$')
-        parser3 = Parser(nord = Nord2Parser(reportHtml), parser_format = r'^[^가-힣]*재무제표$')
-        successor2 = Handler(parser3)
-        successor1 = Handler(parser2, successor2)
-        report = Handler(parser1, successor1).handle_request()
-        return report
+        # parser3 = Parser(nord = Nord2Parser(reportHtml), parser_format = r'^[^가-힣]*재무제표$')
+        # successor2 = Handler(parser3)
+        # successor1 = Handler(parser2, successor2)
+        successor = Handler(parser2)
+        # html = Handler(parser1, successor1).handle_request()
+        html = Handler(parser1, successor).handle_request()
+        return html
+
+class BalanceSheet:
+    # consolidatedParsers = [r'연\s*결\s*재\s*무\s*상\s*태\s*표\s*']
+    # consolidatedParsers = [r'연\s*결\s*손\s*익\s*계\s*산\s*서\s*']
+    # consolidatedParsers = [r'연\s*결\s*포\s*괄\s*손\s*익\s*계\s*산\s*서\s*']
+    consolidatedParsers = [r'연\s*결\s*현\s*금\s*흐\s*름\s*표']
+    notConsolidated = []
+
+    def __init__(self, html):
+        self.soup = html
+        
+    def export_first_numeric_report(self):
+        for report in self.reports:
+            for col in report.columns:
+                test_report = report.loc[:, col]
+                test_report = test_report.fillna(0)
+                test_report = test_report.astype(str)
+                test_report = test_report.str.replace(' ', '')
+                test_report = test_report.str.replace(',', '')
+                test_report = test_report.str.replace('(', '-')
+                test_report = test_report.str.replace(')', '')
+                test_report = test_report.apply(lambda col: pd.to_numeric(col, errors='ignore'))
+                # test_report = test_report.astype('float')
+                print(f'value : {test_report.values}')
+                dtype = test_report.values.dtype
+                print(f'dtype : {dtype}')
+                bool1 = dtype == 'float'
+                bool2 = dtype == 'int64'
+                print(f'float_bool : {bool1}')
+                print(f'int_bool : {bool2}')
+                
+                if dtype == 'float':
+                    return report
+                if dtype =='int64':
+                    return report
+
+    def get_consolidated(self):
+        for parser in self.consolidatedParsers:
+            string = self.soup.find_all(string=re.compile(parser))
+            print(bool(string))
+            if string:
+                print(string)
+                table_soup = self.soup.find_all(string=re.compile(parser))[0].find_all_next('table')
+                self.reports = pd.read_html(str(table_soup))
+                report = self.export_first_numeric_report()
+            else:
+                return None
+            return report
     
-
-
 
 if __name__ == '__main__' :
 
@@ -135,10 +183,13 @@ if __name__ == '__main__' :
     import rceptnoInfo
 
     path = Path.home().joinpath('Desktop', 'dataBackUp(211021)')
+
+    stockList = pd.read_parquet(path/'stockListDB.parquet')
+    tickers = stockList.ticker.unique().tolist()
+    ticker = random.choice(tickers)
+
     commonStockProvider = stockInfo.commonStockProvider()
     stockinfo = stockInfo.StockInfo(path, commonStockProvider)
-
-    ticker = '077970'
     stockInfoDic = stockinfo.get_stockInfo(ticker)
     corp_code = stockInfoDic[ticker]['corp_code']
     print('='*150)
@@ -154,8 +205,11 @@ if __name__ == '__main__' :
     print(f'length of rcept_noLst : {len(rcept_noLst)}')
 
     rcept_no = random.choice(rcept_noLst)
+    # rcept_no = '20160330001826'
     print(f'rcept_no : {rcept_no}')
     print('-'*150)
-    report = Report().get_report(rcept_no)
+    html = HtmlReport().get_report(rcept_no)
     print('-'*150)
-    print(report[0].head(), report[1].head())
+    if not html == None: 
+        report = BalanceSheet(html).get_consolidated()
+        print(report)
